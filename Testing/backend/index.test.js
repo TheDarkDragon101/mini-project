009737f122dbf03598e7c345f5d2a123e9afcb4c
TheDarkDragon101 <100558448+TheDarkDragon101@users.noreply.github.com>
2023-03-06 21:19:30 +0700

    Update index.test.js

--- Testing/backend/index.test.js
+++ Testing/backend/index.test.js
@@ -10,34 +10,12 @@ import request from 'supertest'
       .post('/events')
       .send({
         title: 'Test Event',
-        date:  '2023-03-20',
+        date:  '2023-03-31',
       });
     expect(response.statusCode).toBe(200);
     expect(response.body).toBe('App event success');
   });
-
-   it('should return an error if title is missing', async () => {
-     const response = await request(app)
-       .post('/events')
-       .send({
-         title: '',
-         date:  '2023-03-22',
-      });
-     expect(response.statusCode).toBe(500);
-     expect(response.body.message).toBe("ER_BAD_NULL_ERROR: Column 'title' cannot be null");
-   });
-
-   it('should return an error if date is missing', async () => {
-     const response = await request(app)
-      .post('/events')
-      .send({
-        title: 'Test Event',
-        date:  '',
-       });
-     expect(response.statusCode).toBe(500);
-     expect(response.body.message).toBe("ER_BAD_NULL_ERROR: Column 'date' cannot be null");
-  });
- });
+ })
 
 
 
