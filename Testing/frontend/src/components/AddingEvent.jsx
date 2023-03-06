e9cbeff9bc244cb7b1ac0acd48dfe264dff51f70
TheDarkDragon101 <100558448+TheDarkDragon101@users.noreply.github.com>
2023-03-06 21:18:42 +0700

    Update AddingEvent.jsx

--- Testing/frontend/src/components/AddingEvent.jsx
+++ Testing/frontend/src/components/AddingEvent.jsx
@@ -22,8 +22,8 @@ const AddingEvent = () => {
   return (
 <div className="add-event-wrapper">
   <form action="" className='form-in-adding' onSubmit={handleUpload}>
-    <input type="date" onChange={handleChange} name='date'/>
-    <input type="text" onChange={handleChange} name='title'/>
+    <input type="text" onChange={handleChange} name='date'id="date-add"/>
+    <input type="text" onChange={handleChange} name='title'id="event-add"/>
     {console.log(date)}
     <button type='submit'>submit</button>
     </form>
